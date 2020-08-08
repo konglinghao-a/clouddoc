9b885f888baec50fdeb6e51e3bfb01461f3f6d96
konglinghao <1044325769@qq.com>
2020-08-08 13:17:05 +0800

    修改readme

--- README.md
+++ README.md
@@ -1,68 +1,33 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# 设置文件保存地址
 
-## Available Scripts
+![](./media/设置文档保存位置.png)
 
-In the project directory, you can run:
+# 七牛云配置
 
-### `yarn start`
+![](./media/七牛云配置.png)
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+# 新建文档
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+![](./media/新建.png)
 
-### `yarn test`
+# 编写文档
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+![](./media/编写新建的文档.png)
 
-### `yarn build`
+# 重命名文档
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+![](./media/重命名文档.png)
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+# 搜索文档
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+![](./media/搜索文档.png)
 
-### `yarn eject`
+# 删除文档
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+![](./media/删除文档.png)
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+# 本地导入文档
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+![](./media/本地导入文档.png)
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+![](./media/本地导入文档成功.png)
--- src/App.js
+++ src/App.js
@@ -24,14 +24,14 @@ const settingsStore = new Store({ name: 'Settings' });
 
 //判断要不要自动同步到七牛云
 const getAutoAsync = () => {
-  const qiniuIsConfiged = ['access-key', 'secret-key', 'bucket'].every(id => {
+  const qiniuIsConfiged = ['access-key', 'secret-key', 'bucket'].every((id) => {
     let savedQiniuConfig = settingsStore.get('savedQiniuConfig');
     return !!savedQiniuConfig[id];
-  })
+  });
   // console.log('qiniuisconfig', qiniuIsConfiged);
   // console.log('enableAutoSync', settingsStore.get('enableAutoSync'));
   return qiniuIsConfiged && settingsStore.get('enableAutoSync');
-}
+};
 // console.log(fileStore.delete('files'))
 // console.log(fileStore.get('files'))
 
@@ -45,13 +45,13 @@ const saveFilesToStore = (files) => {
       title,
       createdAt,
       isSynced,
-      updatedAt
-    }
-    return result
+      updatedAt,
+    };
+    return result;
   }, {});
   fileStore.set('files', filesStoreObj);
   console.log(fileStore.get('files'));
-}
+};
 
 function App() {
   const [files, setFiles] = useState(fileStore.get('files') || {});
@@ -63,10 +63,11 @@ function App() {
   const openedFiles = openFileIds.map((openId) => files[openId]);
   const activeFile = files[activeFileId];
   const filesArr = objToArr(files);
-  const fileListArr = (searchedFiles.length > 0) ? searchedFiles : filesArr;
+  const fileListArr = searchedFiles.length > 0 ? searchedFiles : filesArr;
   // const savedLocation = remote.app.getPath('documents')
   // const savedLocation = 'C:/Users/10443/Desktop/mds-and-cloud-doc/mds';
-  const savedLocation = settingsStore.get('savedFilesLocation') || remote.app.getPath('documents');
+  const savedLocation =
+    settingsStore.get('savedFilesLocation') || remote.app.getPath('documents');
 
   const fileClick = (fileId) => {
     // set current active file
@@ -74,19 +75,19 @@ function App() {
     const currentFile = files[fileId];
     const { id, title, path, isLoaded } = currentFile;
 
-    console.log('files:',fileStore.get('files'));
+    console.log('files:', fileStore.get('files'));
     if (!isLoaded) {
       if (getAutoAsync()) {
         ipcRenderer.send('download-file', {
           key: `${title}.md`,
           path,
-          id
-        })
+          id,
+        });
       } else {
-        fileHelper.readFile(path).then(value => {
-          const newFile = { ...files[fileId], body: value, isLoaded: true }
+        fileHelper.readFile(path).then((value) => {
+          const newFile = { ...files[fileId], body: value, isLoaded: true };
           setFiles({ ...files, [fileId]: newFile });
-        })
+        });
       }
     }
     // if openedFiles don't have thie current id
@@ -94,17 +95,17 @@ function App() {
     if (!openFileIds.includes(fileId)) {
       setOpenFileIds([...openFileIds, fileId]);
     }
-  }
+  };
 
   const tabClick = (fileId) => {
     // set current active file
     setActiveFileId(fileId);
     // console.log(settingsStore.get('savedQiniuConfig'));
-  }
+  };
 
   const tabClose = (fileId) => {
     // remove current id from openFileId
-    const tabsWithout = openFileIds.filter(openId => openId !== fileId);
+    const tabsWithout = openFileIds.filter((openId) => openId !== fileId);
     setOpenFileIds(tabsWithout);
     // set the active to the first opened tab if still tabs left
     if (tabsWithout.length > 0) {
@@ -112,7 +113,7 @@ function App() {
     } else {
       setActiveFileId('');
     }
-  }
+  };
 
   const fileChange = (fileId, value) => {
     if (value !== files[fileId].body) {
@@ -120,13 +121,12 @@ function App() {
       setFiles({ ...files, [fileId]: newFile });
       // update unsavedIds
       if (!unsavedFileIds.includes(fileId)) {
-        setUnsavedFileIds([...unsavedFileIds, fileId])
+        setUnsavedFileIds([...unsavedFileIds, fileId]);
       }
     }
-  }
+  };
 
   const deleteFile = async (fileId, title) => {
-
     if (files[fileId].isNew) {
       const { [fileId]: value, ...afterDelete } = files;
       setFiles(afterDelete);
@@ -143,18 +143,17 @@ function App() {
       //close the tab if opened
       tabClose(fileId);
     }
-  }
+  };
 
   //需要新增一个参数来判断是新建还是重命名
   const updateFileName = (fileId, title, isNew) => {
     // newPath should be different based on isNew
     // if isNew is false, path should be old dirname + new title
-    const newPath = isNew ? join(savedLocation, `/${title}.md`) :
-      join(dirname(files[fileId].path), `${title}.md`)
-      ;
-
+    const newPath = isNew
+      ? join(savedLocation, `/${title}.md`)
+      : join(dirname(files[fileId].path), `${title}.md`);
     const newFile = { ...files[fileId], title, isNew: false, path: newPath };
-    const newFiles = { ...files, [fileId]: newFile }
+    const newFiles = { ...files, [fileId]: newFile };
     if (isNew) {
       fileHelper.writeFile(newPath, files[fileId].body).then(() => {
         setFiles(newFiles);
@@ -169,13 +168,13 @@ function App() {
         saveFilesToStore(newFiles);
       });
     }
-  }
+  };
 
   const fileSearch = (keyword) => {
     // filter out the new files based on the keyword
-    const newFiles = filesArr.filter(file => file.title.includes(keyword));
+    const newFiles = filesArr.filter((file) => file.title.includes(keyword));
     setSearchedFiles(newFiles);
-  }
+  };
 
   const createNewFile = () => {
     const newId = uuidv4();
@@ -184,88 +183,98 @@ function App() {
       title: '',
       body: '',
       createdAt: Date.now(),
-      isNew: true
-    }
+      isNew: true,
+    };
     setFiles({ ...files, [newId]: newFile });
-  }
+  };
 
   const saveCurrentFile = () => {
     const { path, body, title } = activeFile;
-    console.log('active',activeFile);
+    console.log('active', activeFile);
     fileHelper.writeFile(path, body).then(() => {
-      setUnsavedFileIds(unsavedFileIds.filter(id => id !== activeFile.id));
+      setUnsavedFileIds(unsavedFileIds.filter((id) => id !== activeFile.id));
     });
     if (getAutoAsync()) {
-      ipcRenderer.send('upload-file', { key: `${title}.md`, path })
+      ipcRenderer.send('upload-file', { key: `${title}.md`, path });
     }
-  }
+  };
 
   const importFiles = () => {
-    remote.dialog.showOpenDialog({
-      title: '选择导入的Markdown文件',
-      // properties可以选择是否能传文件以及文件夹等等。
-      properties: ['openFile', 'multiSelections'],
-      //规定可选类型
-      filters: [
-        { name: 'Markdown files', extensions: ['md'] }
-      ]
-    }).then((result) => {
-      // console.log(result.filePaths);
-      if (result.filePaths.length > 0) {
-        //filter out the path we already have in electron store
-        const filteredPaths = result.filePaths.filter(path => {
-          const alreadyAdded = Object.values(files).find(file => {
-            return file.path === path
-          })
-          return !alreadyAdded;
-        })
-        //extend the path array to an array contains files info
-        const importFileArr = filteredPaths.map(path => {
-          return {
-            id: uuidv4(),
-            title: basename(path, extname(path)),
-            path
+    remote.dialog
+      .showOpenDialog({
+        title: '选择导入的Markdown文件',
+        // properties可以选择是否能传文件以及文件夹等等。
+        properties: ['openFile', 'multiSelections'],
+        //规定可选类型
+        filters: [{ name: 'Markdown files', extensions: ['md'] }],
+      })
+      .then((result) => {
+        // console.log(result.filePaths);
+        if (result.filePaths.length > 0) {
+          //filter out the path we already have in electron store
+          const filteredPaths = result.filePaths.filter((path) => {
+            const alreadyAdded = Object.values(files).find((file) => {
+              return file.path === path;
+            });
+            return !alreadyAdded;
+          });
+          //extend the path array to an array contains files info
+          const importFileArr = filteredPaths.map((path) => {
+            return {
+              id: uuidv4(),
+              title: basename(path, extname(path)),
+              path,
+            };
+          });
+          // get the new files object in flattenArr
+          const newFiles = { ...files, ...flattenArr(importFileArr) };
+          //setState and update electron store
+          setFiles(newFiles);
+          saveFilesToStore(newFiles);
+          if (importFileArr.length > 0) {
+            remote.dialog.showMessageBox({
+              type: 'info',
+              title: `成功导入${importFileArr.length}文件！`,
+              message: `成功导入${importFileArr.length}文件！`,
+            });
           }
-        })
-        // get the new files object in flattenArr
-        const newFiles = { ...files, ...flattenArr(importFileArr) };
-        //setState and update electron store
-        setFiles(newFiles);
-        saveFilesToStore(newFiles);
-        if (importFileArr.length > 0) {
-          remote.dialog.showMessageBox({
-            type: 'info',
-            title: `成功导入${importFileArr.length}文件！`,
-            message: `成功导入${importFileArr.length}文件！`
-          })
         }
-      }
-    })
-  }
+      });
+  };
 
   const activeFileUploaded = () => {
     const { id } = activeFile;
-    const modifiedFile = { ...files[id], isSynced: true, updatedAt: new Date().getTime()}
-    const newFiles = { ...files, [id]: modifiedFile};
+    const modifiedFile = {
+      ...files[id],
+      isSynced: true,
+      updatedAt: new Date().getTime(),
+    };
+    const newFiles = { ...files, [id]: modifiedFile };
     setFiles(newFiles);
     saveFilesToStore(newFiles);
-  }
+  };
 
   const activeFileDownloaded = (event, message) => {
     const currentFile = files[message.id];
     const { id, path } = currentFile;
-    fileHelper.readFile(path).then(value => {
+    fileHelper.readFile(path).then((value) => {
       let newFile;
       if (message.status === 'download-success') {
-        newFile = { ...files[id], body: value, isLoaded: true, isSynced: true, updatedAt: new Date().getTime()};
+        newFile = {
+          ...files[id],
+          body: value,
+          isLoaded: true,
+          isSynced: true,
+          updatedAt: new Date().getTime(),
+        };
       } else {
-        newFile = { ...files[id], body: value, isLoaded: true};
+        newFile = { ...files[id], body: value, isLoaded: true };
       }
       const newFiles = { ...files, [id]: newFile };
       setFiles(newFiles);
       saveFilesToStore(newFiles);
-    })
-  }
+    });
+  };
   //上传全部文件以后主要是要更新updateAt的值
   const filesUpLoaded = () => {
     const newFiles = objToArr(files).reduce((result, file) => {
@@ -273,13 +282,13 @@ function App() {
       result[file.id] = {
         ...files[file.id],
         isSynced: true,
-        updatedAt: currentTime
-      }
+        updatedAt: currentTime,
+      };
       return result;
     }, {});
     setFiles(newFiles);
     saveFilesToStore(newFiles);
-  }
+  };
   useIpcRenderer({
     'create-new-file': createNewFile,
     'import-file': importFiles,
@@ -287,53 +296,47 @@ function App() {
     'active-file-uploaded': activeFileUploaded,
     'file-downloaded': activeFileDownloaded,
     'files-uploaded': filesUpLoaded,
-    'loading-status': (massage, status) => { setIsLoading(status) }
-  })
+    'loading-status': (massage, status) => {
+      setIsLoading(status);
+    },
+  });
 
   return (
-    <div className="App container-fluid px-0">
-      { isLoading && 
-        <Loader />
-      }
-      <div className="row no-gutters">
-        <div className="col-3 left-panel left-panel">
-          <FileSearch
-            title='我的云文档'
-            onFileSearch={fileSearch}
-          />
+    <div className='App container-fluid px-0'>
+      {isLoading && <Loader />}
+      <div className='row no-gutters'>
+        <div className='col-3 left-panel left-panel'>
+          <FileSearch title='我的云文档' onFileSearch={fileSearch} />
           <FileList
             files={fileListArr}
             onFileClick={fileClick}
             onFileDelete={deleteFile}
             onSaveEdit={updateFileName}
           />
-          <div className="row no-gutters button-group">
-            <div className="col">
+          <div className='row no-gutters button-group'>
+            <div className='col'>
               <BottomBtn
-                text="新建"
-                colorClass="btn-primary"
+                text='新建'
+                colorClass='btn-primary'
                 icon={faPlus}
                 onBtnClick={createNewFile}
               />
             </div>
-            <div className="col">
+            <div className='col'>
               <BottomBtn
-                text="导入"
-                colorClass="btn-success"
+                text='导入'
+                colorClass='btn-success'
                 icon={faFileImport}
                 onBtnClick={importFiles}
               />
             </div>
           </div>
         </div>
-        <div className="col-9 right-panel">
-          {!activeFile &&
-            <div className="start-page">
-              选择或者创建新的 markdown 文档
-            </div>
-          }
-          {
-            activeFile &&
+        <div className='col-9 right-panel'>
+          {!activeFile && (
+            <div className='start-page'>选择或者创建新的 markdown 文档</div>
+          )}
+          {activeFile && (
             <>
               <TabList
                 files={openedFiles}
@@ -345,18 +348,21 @@ function App() {
               <SimpleMDE
                 key={activeFile && activeFile.id} //在读取不同的file的时候要有不同的key
                 value={activeFile && activeFile.body} //markdown里显示的内容
-                onChange={(value) => { fileChange(activeFile.id, value) }} //改变内容时的回调
+                onChange={(value) => {
+                  fileChange(activeFile.id, value);
+                }} //改变内容时的回调
                 options={{
-                  minHeight: '515px'
+                  minHeight: '515px',
                 }}
               />
-              { activeFile.isSynced && 
-                <span className="sync-status">
-                  已同步，上次同步时间: {timestampToString(activeFile.updatedAt)}
+              {activeFile.isSynced && (
+                <span className='sync-status'>
+                  已同步，上次同步时间:{' '}
+                  {timestampToString(activeFile.updatedAt)}
                 </span>
-              }
+              )}
             </>
-          }
+          )}
         </div>
       </div>
     </div>
